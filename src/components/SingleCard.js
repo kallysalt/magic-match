441ffce5c7043101c1dec7eb350b2a444767a766
Zhubo Zhou <zhubozho@gmail.com>
2022-07-04 00:14:06 -0700

    clean up

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -13,7 +13,7 @@ export default function SingleCard({card, handleChoice, flipped, disabled}) {
             <div className = {flipped ? "flipped" : ""}>
                 <img className="front" src={card.src} alt="card front" />
                 <img 
-                    className="back" src="/img/cover.png" onClick={handleClick} alt="card back" />
+                    className="back" src="./img/cover.png" onClick={handleClick} alt="card back" />
             </div>
         </div>
     )

976095f2aba185c974d280655bcf202c5138179a
Zhubo Zhou <zhubozho@gmail.com>
2022-07-03 23:40:56 -0700

    Updates

---
+++ precache-manifest.cf542baae21fdc0b97aea940249026d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cfa9f857aaa92e462f1",
+    "url": "/magic-match/static/css/main.4ed24581.chunk.css"
+  },
+  {
+    "revision": "9cfa9f857aaa92e462f1",
+    "url": "/magic-match/static/js/main.e8870a7b.chunk.js"
+  },
+  {
+    "revision": "70e8e6405c369f580869",
+    "url": "/magic-match/static/js/runtime~main.e3d21a2e.js"
+  },
+  {
+    "revision": "692124210539fe1989d3",
+    "url": "/magic-match/static/js/2.477a3c98.chunk.js"
+  },
+  {
+    "revision": "ede9d04752f49b9c5b23ca8d770667e9",
+    "url": "/magic-match/index.html"
+  }
+];
